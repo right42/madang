67a102b8e69fecfd264bdd933e145cc9bb44618d
akakanslzk2 <40883161+akakanslzk2@users.noreply.github.com>
2020-05-21 20:42:14 +0900

    첫세팅

---
+++ madang/src/main/java/sijang/project/madang/MadangApplication.java
@@ -0,0 +1,13 @@
+package sijang.project.madang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadangApplication.class, args);
+	}
+
+}
---
+++ madang/src/test/java/sijang/project/madang/MadangApplicationTests.java
@@ -0,0 +1,13 @@
+package sijang.project.madang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
